241cb7f904b9fa3589817ef265965a095f56c69d
ZSM <maynard.zach23@gmail.com>
2023-03-11 13:36:54 -0500

    placeholder text on routed dev pages

--- src/Components/Splash/Pages/Dev/Other/Other.js
+++ src/Components/Splash/Pages/Dev/Other/Other.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Other = props => {
   return (
-    <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: 'auto', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'scroll'} : props.theme === 'dark' ? {backgroundColor: '#222', height: 'auto', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'scroll'} : {} }>
-      Other
+    <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'scroll', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'scroll', color: 'whitesmoke'} : {} }>
+      This page should be finished soon. Check back in a bit!
     </div>
   )
 }
