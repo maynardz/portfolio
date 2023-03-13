d4a26ec9912cc229c55a63f286aef445e1d85ddf
ZSM <maynard.zach23@gmail.com>
2023-03-13 16:21:39 -0400

    re-insert placeholder content - no time to finish components right now

--- src/Components/Splash/Pages/Dev/DeepSeas/DeepSeas.js
+++ src/Components/Splash/Pages/Dev/DeepSeas/DeepSeas.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import './DeepSeas.css';
 
-import ds_demo from '../../../../../assets/video/ds_demo.mp4';
-import webflow_icon from '../../../../../assets/images/webflow_icon.png';
+// import ds_demo from '../../../../../assets/video/ds_demo.mp4';
+// import webflow_icon from '../../../../../assets/images/webflow_icon.png';
 
 const DeepSeas = props => {
   return (
-    <div className='deepseas-container' style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', color: 'whitesmoke'} : {} } >
-      <div>
-        <p className='deepseas-heading'>DeepSeas</p>
-        <div className='sw-container'>
-          <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
-          <div className='ds_icon_container'>
-            <img src={webflow_icon} style={{ height: '24px', width: 'auto'}} />
-            <box-icon color={'#737070'} name='javascript' type='logo' ></box-icon>
-            <box-icon color={'#737070'} name='html5' type='logo'></box-icon>
-            <box-icon color={'#737070'} name='css3' type='logo' ></box-icon>
-          </div>
-          <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
-        </div>
-      </div>
-      
-      <div className='deepseas-info-container'>
-        <div className='content-container'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> 
-        </div>
-        <div className='video-container'>
-          <video className='video-tag' autoPlay loop muted style={{height: '100%', width: '100%'}}>
-            <source src={ds_demo} type='video/mp4' />
-          </video>
-        </div>
-      </div>
+    <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'whitesmoke'} : {} }>
+      This page should be finished soon. Check back in a bit!
     </div>
+    // <div className='deepseas-container' style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', color: 'whitesmoke'} : {} } >
+    //   <div>
+    //     <p className='deepseas-heading'>DeepSeas</p>
+    //     <div className='sw-container'>
+    //       <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
+    //       <div className='ds_icon_container'>
+    //         <img src={webflow_icon} style={{ height: '24px', width: 'auto'}} />
+    //         <box-icon color={'#737070'} name='javascript' type='logo' ></box-icon>
+    //         <box-icon color={'#737070'} name='html5' type='logo'></box-icon>
+    //         <box-icon color={'#737070'} name='css3' type='logo' ></box-icon>
+    //       </div>
+    //       <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
+    //     </div>
+    //   </div>
+      
+    //   <div className='deepseas-info-container'>
+    //     <div className='content-container'>
+    //       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> 
+    //     </div>
+    //     <div className='video-container'>
+    //       <video className='video-tag' autoPlay loop muted style={{height: '100%', width: '100%'}}>
+    //         <source src={ds_demo} type='video/mp4' />
+    //       </video>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
--- src/Components/Splash/Pages/Dev/GreeneStreet/GreeneStreet.js
+++ src/Components/Splash/Pages/Dev/GreeneStreet/GreeneStreet.js
@@ -1,44 +1,47 @@
 import React from 'react';
 import './GreeneStreet.css';
 
-import gs_demo from '../../../../../assets/video/gs_demo.mp4';
+// import gs_demo from '../../../../../assets/video/gs_demo.mp4';
 
 const GreeneStreet = props => {
 
-  React.useEffect(() => {
-    let video = document.querySelector('.video-tag');
+  // React.useEffect(() => {
+  //   let video = document.querySelector('.video-tag');
 
-    video.playbackRate = 2.5;
-  }, []);
+  //   video.playbackRate = 2.5;
+  // }, []);
 
   return (
-    <div className='gs-container' style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', color: 'whitesmoke'} : {} } >
-      <div>
-        <p className='gs-heading'>Greene Street</p>
-        <div className='sw-container'>
-          <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
-          <div className='ds_icon_container'>
-            <box-icon color={'#737070'} name='react' type='logo'></box-icon>
-            <box-icon color={'#737070'} name='nodejs' type='logo' ></box-icon>
-            <box-icon color={'#737070'} name='postgresql' type='logo' ></box-icon>
-            <box-icon color={'#737070'} type='logo' name='aws'></box-icon>
-          </div>
-          <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
-        </div>
-      </div>
-      
-      <div className='gs-info-container'>
-        <div className='content-container'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> 
-        </div>
-        <div className='video-container'>
-          <video className='video-tag' autoPlay loop muted style={{height: '100%', width: '100%'}}>
-            <source src={gs_demo} type='video/mp4' />
-          </video>
-        </div>
-      </div>
-       
+    <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'whitesmoke'} : {} }>
+      This page should be finished soon. Check back in a bit!
     </div>
+    // <div className='gs-container' style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', color: 'whitesmoke'} : {} } >
+    //   <div>
+    //     <p className='gs-heading'>Greene Street</p>
+    //     <div className='sw-container'>
+    //       <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
+    //       <div className='ds_icon_container'>
+    //         <box-icon color={'#737070'} name='react' type='logo'></box-icon>
+    //         <box-icon color={'#737070'} name='nodejs' type='logo' ></box-icon>
+    //         <box-icon color={'#737070'} name='postgresql' type='logo' ></box-icon>
+    //         <box-icon color={'#737070'} type='logo' name='aws'></box-icon>
+    //       </div>
+    //       <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
+    //     </div>
+    //   </div>
+      
+    //   <div className='gs-info-container'>
+    //     <div className='content-container'>
+    //       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> 
+    //     </div>
+    //     <div className='video-container'>
+    //       <video className='video-tag' autoPlay loop muted style={{height: '100%', width: '100%'}}>
+    //         <source src={gs_demo} type='video/mp4' />
+    //       </video>
+    //     </div>
+    //   </div>
+       
+    // </div>
   )
 }
 
