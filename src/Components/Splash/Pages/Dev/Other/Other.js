fae2ae79b34c521e6076680388fec4e6d91241d1
ZSM <maynard.zach23@gmail.com>
2023-03-13 11:54:16 -0400

    all scrollbars removed

--- src/Components/Splash/Pages/Dev/Other/Other.js
+++ src/Components/Splash/Pages/Dev/Other/Other.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Other = props => {
   return (
-    <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'scroll', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'scroll', color: 'whitesmoke'} : {} }>
+    <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'whitesmoke'} : {} }>
       This page should be finished soon. Check back in a bit!
     </div>
   )
