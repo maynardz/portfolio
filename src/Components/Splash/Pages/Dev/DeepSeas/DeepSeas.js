72b14ef60f5bfb695aab197fcd61d94ee78074d5
maynardz <maynard.zach23@gmail.com>
2026-04-27 11:03:42 -0400

    build errors

--- src/Components/Splash/Pages/Dev/DeepSeas/DeepSeas.js
+++ src/Components/Splash/Pages/Dev/DeepSeas/DeepSeas.js
@@ -14,7 +14,7 @@ const DeepSeas = props => {
     vid.addEventListener('loadeddata', () => {
       setLoaded(!loaded);
     })
-  }, []);
+  }, [loaded]);
   
   return (
     // <div style={ props.theme === 'light' ? {backgroundColor: 'whitesmoke', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: '#222'} : props.theme === 'dark' ? {backgroundColor: '#222', height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'whitesmoke'} : {} }>
@@ -26,7 +26,7 @@ const DeepSeas = props => {
         <div className='sw-container'>
           <hr className='style-line' style={ props.theme === 'light' ? {backgroundColor: '#222'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke'} : {}} />
           <div className='ds_icon_container'>
-            <img src={webflow_icon} style={{ height: '24px', width: 'auto'}} />
+            <img src={webflow_icon} style={{ height: '24px', width: 'auto'}} alt="webflow icon" />
             <box-icon color={'#737070'} name='javascript' type='logo' ></box-icon>
             <box-icon color={'#737070'} name='html5' type='logo'></box-icon>
             <box-icon color={'#737070'} name='css3' type='logo' ></box-icon>
@@ -56,7 +56,7 @@ const DeepSeas = props => {
           <video className='video-tag' autoPlay loop muted style={loaded ? {height: '100%', width: '100%'} : { display: 'none' }}>
             <source src={'https://res.cloudinary.com/maynardz/video/upload/v1678980372/Photography/ds_demo_ntockc.mp4'} type='video/mp4'  />
           </video>
-          <button style={ props.theme === 'light' ? {backgroundColor: '#222', color: 'whitesmoke', fontFamily: 'rejova', border: 'none', padding: '1em'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke', color: '#222', fontFamily: 'rejova', border: 'none', padding: '1em'} : {} }><a href='https://deepseas.webflow.io/' target='_blank' style={ props.theme === 'light' ? {color: 'whitesmoke'} : props.theme === 'dark' ? {color: '#222'} : {} }>Site</a></button>
+          <button style={ props.theme === 'light' ? {backgroundColor: '#222', color: 'whitesmoke', fontFamily: 'rejova', border: 'none', padding: '1em'} : props.theme === 'dark' ? {backgroundColor: 'whitesmoke', color: '#222', fontFamily: 'rejova', border: 'none', padding: '1em'} : {} }><a href='https://deepseas.webflow.io/' target='_blank' rel="noreferrer" style={ props.theme === 'light' ? {color: 'whitesmoke'} : props.theme === 'dark' ? {color: '#222'} : {} }>Site</a></button>
         </div>
       </div>
     </div>
