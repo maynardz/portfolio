0f162b08fd4180cd616588b850b06c699cc6469c
maynardz <maynard.zach23@gmail.com>
2026-04-27 11:18:20 -0400

    fix landing animation

--- src/Components/Splash/Pages/Landing/Landing.js
+++ src/Components/Splash/Pages/Landing/Landing.js
@@ -1,106 +1,62 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import './Landing.css';
 
-import { useRef } from 'react';
-
 import anime from 'animejs/lib/anime.es.js';
-import Letterize from "letterizejs/lib/letterize.js";
+import Letterize from 'letterizejs/lib/letterize.js';
 
-import {
-  // useScroll,
-  // useTransform,
-} from "framer-motion";
-
-// function useParallax(value, distance) {
-//   return useTransform(value, [0, 1], [-distance, distance]);
-// };
-
-const Landing = props => {
+const Landing = () => {
   const ref = useRef(null);
-  // const { scrollYProgress } = useScroll({ target: ref });
-  // const y = useParallax(scrollYProgress, 300);
+  const animationRef = useRef(null);
 
-  const targetList = new Letterize({
-    targets: ".animate"
-  });
+  useLayoutEffect(() => {
+    const targetList = new Letterize({
+      targets: '.animate',
+    });
 
-  const [targets, setTargets] = React.useState(false);
-  const [animationRef, setAnimationRef] = React.useState(anime);
+    if (!targetList?.listAll?.length || !targetList?.list?.length) {
+      console.log('no targets');
+      return undefined;
+    }
 
-  React.useEffect(() => {
-    // console.log(targetList);
-    setTargets(true);
-  }, []);
-
-  React.useLayoutEffect(() => {
-    targets ? (
-      setAnimationRef(
-        anime.timeline({
+    animationRef.current = anime
+      .timeline({
         targets: targetList.listAll,
         delay: anime.stagger(100, {
           grid: [targetList.list[0].length, targetList.list.length],
-          from: "center"
-          }),
-        loop: true
-        }).add({
-          scale: 0.5
-        })
-        .add({
-          letterSpacing: "25px"
-        })
-        .add({
-          scale: 1
-        })
-        .add({
-          letterSpacing: "10px"
-        })
-      )
-    ) : console.log('no targets');
-  }, [targets]);
+          from: 'center',
+        }),
+        loop: true,
+      })
+      .add({ scale: 0.5 })
+      .add({ letterSpacing: '25px' })
+      .add({ scale: 1 })
+      .add({ letterSpacing: '10px' });
+
+    return () => {
+      if (animationRef.current) {
+        animationRef.current.pause();
+      }
+    };
+  }, []);
 
   return (
-    <div style={{marginLeft: '0.5em', marginTop: '-2.5em'}}>
+    <div style={{ marginLeft: '0.5em', marginTop: '-2.5em' }}>
       <div ref={ref}>
-        <div className="animate">
-            ZSM.DEV
-        </div>
-        <div className="animate">
-            DEV.ZSM
-        </div>
-        <div className="animate">
-            ZSM.DEV
-        </div>
-        <div className="animate">
-            DEV.ZSM
-        </div>
-        <div className="animate">
-            ZSM.DEV
-        </div>
-        <div className="animate">
-            DEV.ZSM
-        </div>
-        <div className="animate">
-            ZSM.DEV
-        </div>
-        <div className="animate">
-            DEV.ZSM
-        </div>
-        <div className="animate">
-            ZSM.DEV
-        </div>
-        <div className="animate">
-            DEV.ZSM
-        </div>
-        <div className="animate">
-            ZSM.DEV
-        </div>
-        <div className="animate">
-            DEV.ZSM
-        </div>
+        <div className="animate">ZSM.DEV</div>
+        <div className="animate">DEV.ZSM</div>
+        <div className="animate">ZSM.DEV</div>
+        <div className="animate">DEV.ZSM</div>
+        <div className="animate">ZSM.DEV</div>
+        <div className="animate">DEV.ZSM</div>
+        <div className="animate">ZSM.DEV</div>
+        <div className="animate">DEV.ZSM</div>
+        <div className="animate">ZSM.DEV</div>
+        <div className="animate">DEV.ZSM</div>
+        <div className="animate">ZSM.DEV</div>
+        <div className="animate">DEV.ZSM</div>
       </div>
-      {/* <motion.h6 style={{ y }}>{`#001`}</motion.h6> */}
     </div>
-  )
+  );
 };
 
 export default Landing;
